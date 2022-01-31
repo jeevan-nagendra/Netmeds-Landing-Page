2d39665e7c7f0c1a5cc5598ff689bf4c70284633
jeevannagendra <jeevannaagendra@gmail.com>
2022-01-31 17:19:09 +0530

    Netmeds landing page

---
+++ src/components/navbarcomponents/Medicine.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ShowProducts from '../../ShowProducts'
+
+const Medicine = () => {
+    return (
+        <div>
+            <ShowProducts/>
+        </div>
+    )
+}
+
+export default Medicine
---
+++ src/components/navbarcomponents/Wellness.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Wellness = () => {
+    return (
+        <div>
+            <h1>wellness</h1>
+        </div>
+    )
+}
+
+export default Wellness
